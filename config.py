d7a861e73be5fe46d0cbec42863f298dc930a99f
cheruden <cheruden25@gmail.com>
2021-06-14 10:44:41 +0300

    add config class

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_USERNAME= 'cheruden25@gmail.com'
+    MAIL_PASSWORD= '0723056032Aa'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:cheru001@localhost/minutepitch'
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:cheru001@localhost/minutepitch'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    # 'test': TestConfig
+}
